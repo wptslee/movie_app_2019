7f8e8e62c0f63b7c12812df77c604c00dc1e163d
tslee <tslee3375@naver.com>
2021-02-24 21:30:53 +0900

    #4.5 Cutting the summary

--- src/App.js
+++ src/App.js
@@ -1,8 +1,67 @@
-//import React from "react";
+import React from "react";
+//import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
 
-function App() {
-  //return <div className='App' />;
-  return <div>Hello!!!!</div>;
+import "./App.css";
+
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  // componentDidMount() {}
+  // componentDidUpdate() {}
+  // componentWillUnmount() {}
+  // render() {}
+
+  state = {
+    isLoaded: false,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating",
+    );
+    //console.log(movieList);
+    //this.setState({ movies: movies });
+    this.setState({ movies, isLoaded: true });
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoaded, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoaded ? (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        ) : (
+          <div className="loader">
+            <span className="loader__text">Loading ...</span>
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
